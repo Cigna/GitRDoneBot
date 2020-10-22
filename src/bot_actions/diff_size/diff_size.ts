1c74d0acf327fffd2e56d6a1b2befae9efccd5b4
Lanae BK <lanae.blethenkawa@cigna.com>
2020-10-22 18:43:42 -0400

    saving WIP on custom diff parser

--- src/bot_actions/diff_size/diff_size.ts
+++ src/bot_actions/diff_size/diff_size.ts
@@ -90,11 +90,25 @@ export class DiffSize extends BotAction {
         if (change.hasOwnProperty("diff")) {
           const gitDiff = change.diff;
           // workaround for GitLab API 11.2.3 change that broke the schema expected by parse-diff module
-          const hackedDiff = "--- a/README.md\n+++ b/README.md\n" + gitDiff;
+          const hackedDiff = "--- a/file\n+++ b/file\n" + gitDiff;
           const files = parse(hackedDiff);
+          this.customParser(gitDiff);
           const total = files.map((file) => {
-            return Math.abs(file.deletions) + Math.abs(file.additions);
+            const calculatedTotal =
+              Math.abs(file.deletions) + Math.abs(file.additions);
+            // console.log({
+            //   old_path: change.old_path,
+            //   new_path: change.new_path,
+            //   new_file: change.new_file,
+            //   renamed_file: change.renamed_file,
+            //   deleted_file: change.deleted_file,
+            //   calculatedTotal: calculatedTotal,
+            //   deletions: file.deletions,
+            //   additions: file.additions,
+            // });
+            return calculatedTotal;
           });
+
           return total.reduce(
             (accumulator, currentVal) => accumulator + currentVal,
           );
@@ -103,11 +117,22 @@ export class DiffSize extends BotAction {
         }
       });
 
+      console.log(`diffs obj: ${JSON.stringify(diffs, null, " ")}`);
       totalDiffs = diffs.reduce(
         (accumulator, currentVal) => accumulator + currentVal,
       );
     }
 
+    console.log(`totalDiffs: ${totalDiffs}`);
     return totalDiffs;
+  }
+
+  private static customParser(diff: string): void {
+    const diffNewlines: Array<string> = diff.split("/n");
+    console.log(`diffNewlines: ${diffNewlines}`);
+    const noContextLines: Array<string> = diffNewlines.filter((line) => {
+      return line.match(new RegExp(/^\s*[\-\+]/m));
+    });
+    console.log(`noContextLines: ${noContextLines}`);
   }
 }
